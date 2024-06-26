e36229f66796ecb575e35610aacec9979052337d
Houchen Li <houchen_li@hotmail.com>
2024-06-26 17:43:06 +0800

    [Base] add third_party dependencies

---
+++ cmake/third_party.cmake
@@ -0,0 +1,100 @@
+CPMAddPackage(
+  NAME cxxopts
+  GITHUB_REPOSITORY "jarro2783/cxxopts"
+  GIT_TAG "v3.2.1"
+  OPTIONS
+    "CXXOPTS_USE_UNICODE_HELP ON"
+)
+
+CPMAddPackage(
+  NAME spdlog
+  GITHUB_REPOSITORY "gabime/spdlog"
+  GIT_TAG "v1.14.1"
+  OPTIONS
+    "SPDLOG_ENABLE_PCH ON"
+    "SPDLOG_BUILD_PIC ON"
+    "SPDLOG_TIDY OFF"
+    "SPDLOG_USE_STD_FORMAT ON"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+)
+
+if(spdlog_ADDED)
+  target_compile_definitions(spdlog INTERFACE SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_TRACE)
+  target_compile_definitions(spdlog_header_only INTERFACE SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_TRACE)
+endif()
+
+CPMAddPackage(
+  NAME doctest
+  GITHUB_REPOSITORY "doctest/doctest"
+  GIT_TAG "v2.4.9"
+  OPTIONS
+    "DOCTEST_WITH_MAIN_IN_STATIC_LIB ON"
+    "DOCTEST_NO_INSTALL ON"
+    "DOCTEST_USE_STD_HEADERS ON"
+)
+
+if(doctest_ADDED)
+  target_compile_definitions(doctest INTERFACE DOCTEST_CONFIG_SUPER_FAST_ASSERTS)
+endif()
+
+CPMAddPackage(
+  NAME nlohmann_json
+  GITHUB_REPOSITORY "nlohmann/json"
+  GIT_TAG "v3.11.2"
+)
+
+CPMAddPackage(
+  NAME msft_proxy
+  GITHUB_REPOSITORY "microsoft/proxy"
+  GIT_TAG "9636e15b359bba6ce588412f4db15661a39b8c47"
+  OPTIONS
+    "BUILD_TESTING OFF"
+)
+
+find_package(Boost 1.79.0 REQUIRED
+  COMPONENTS
+    serialization
+)
+
+CPMAddPackage(
+  NAME Eigen3
+  GITLAB_REPOSITORY "libeigen/eigen"
+  GIT_TAG "3.4.0"
+  OPTIONS
+    "BUILD_TESTING OFF"
+)
+
+CPMAddPackage(
+  NAME autodiff
+  GITHUB_REPOSITORY "autodiff/autodiff"
+  GIT_TAG "v1.1.2"
+  OPTIONS
+    "AUTODIFF_BUILD_TESTS OFF"
+    "AUTODIFF_BUILD_PYTHON OFF"
+    "AUTODIFF_BUILD_EXAMPLES OFF"
+    "AUTODIFF_BUILD_DOCS OFF"
+)
+
+CPMAddPackage(
+  NAME osqp
+  GITHUB_REPOSITORY "osqp/osqp"
+  GIT_TAG "master"
+  OPTIONS
+    "QDLDL_LONG OFF"
+    "QDLDL_FLOAT OFF"
+    "OSQP_USE_LONG OFF"
+    "OSQP_USE_FLOAT OFF"
+    "OSQP_BUILD_DEMO_EXE OFF"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+)
+
+CPMAddPackage(
+  NAME Matplot++
+  GITHUB_REPOSITORY "alandefreitas/matplotplusplus"
+  GIT_TAG "v1.2.1"
+  OPTIONS
+    "CMAKE_INTERPROCEDURAL_OPTIMIZATION ON"
+    "CMAKE_CXX_STANDARD 17"
+    "MATPLOTPP_BUILD_WITH_PEDANTIC_WARNINGS OFF"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+)
