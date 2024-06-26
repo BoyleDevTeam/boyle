037184b482b142d66d1674fb5fa236b63d420858
Houchen Li <houchen_li@hotmail.com>
2024-06-26 17:43:06 +0800

    [Base] add third_party dependencies

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -19,3 +19,19 @@ function(install_libraries)
   )
   unset(current_subdirectory)
 endfunction()
+
+function(add_doctest_test name source_name)
+  set(test_name ${name}_test)
+  add_executable(${test_name} ${source_name})
+  target_link_libraries(${test_name}
+    PRIVATE
+      cxxopts::cxxopts
+      doctest::doctest
+      Matplot++::matplot
+      ${name}
+  )
+  add_test(
+    NAME ${test_name}
+    COMMAND ${test_name}
+  )
+endfunction()
