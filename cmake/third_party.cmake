615548460f769989452652a48eb17bef4c9803c3
Houchen Li <houchen_li@hotmail.com>
2024-06-26 17:43:06 +0800

    [Base] add third_party dependencies

---
+++ cmake/third_party.cmake
@@ -0,0 +1,94 @@
+CPMAddPackage(
+  NAME cxxopts
+  GITHUB_REPOSITORY "jarro2783/cxxopts"
+  GIT_TAG "v3.2.1"
+)
+
+CPMAddPackage(
+  NAME spdlog
+  GITHUB_REPOSITORY "gabime/spdlog"
+  GIT_TAG "v1.14.1"
+  OPTIONS
+    "SPDLOG_ENABLE_PCH ON"
+    "SPDLOG_BUILD_PIC ON"
+    "SPDLOG_TIDY OFF"
+    "SPDLOG_USE_STD_FORMAT ON"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+)
+
+add_compile_definitions(SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_TRACE)
+
+CPMAddPackage(
+  NAME doctest
+  GITHUB_REPOSITORY "doctest/doctest"
+  GIT_TAG "v2.4.9"
+  OPTIONS
+    "DOCTEST_WITH_MAIN_IN_STATIC_LIB ON"
+    "DOCTEST_NO_INSTALL ON"
+    "DOCTEST_USE_STD_HEADERS ON"
+)
+
+add_compile_definitions(DOCTEST_CONFIG_SUPER_FAST_ASSERTS)
+
+# CPMAddPackage(
+#   NAME nlohmann_json
+#   GITHUB_REPOSITORY "nlohmann/json"
+#   GIT_TAG "v3.11.2"
+# )
+
+CPMAddPackage(
+  NAME msft_proxy
+  GITHUB_REPOSITORY "microsoft/proxy"
+  GIT_TAG "9636e15b359bba6ce588412f4db15661a39b8c47"
+  OPTIONS
+    "BUILD_TESTING OFF"
+)
+
+find_package(Boost 1.79.0 REQUIRED
+  COMPONENTS
+    serialization
+)
+
+# CPMAddPackage(
+#   NAME OpenBLAS
+#   GITHUB_REPOSITORY "OpenMathLib/OpenBLAS"
+#   GIT_TAG "v0.3.27"
+#   OPTIONS
+#     "BUILD_STATIC_LIBS ON"
+# )
+
+# CPMAddPackage(
+#   NAME autodiff
+#   GITHUB_REPOSITORY "autodiff/autodiff"
+#   GIT_TAG "v1.1.2"
+#   OPTIONS
+#     "AUTODIFF_BUILD_TESTS OFF"
+#     "AUTODIFF_BUILD_PYTHON OFF"
+#     "AUTODIFF_BUILD_EXAMPLES OFF"
+#     "AUTODIFF_BUILD_DOCS OFF"
+# )
+
+CPMAddPackage(
+  NAME osqp
+  GITHUB_REPOSITORY "osqp/osqp"
+  GIT_TAG "master"
+  OPTIONS
+    "QDLDL_LONG OFF"
+    "QDLDL_FLOAT OFF"
+    "OSQP_USE_LONG OFF"
+    "OSQP_USE_FLOAT OFF"
+    "OSQP_BUILD_DEMO_EXE OFF"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+    "CMAKE_POLICY_DEFAULT_CMP0169 OLD"
+)
+
+CPMAddPackage(
+  NAME Matplot++
+  GITHUB_REPOSITORY "alandefreitas/matplotplusplus"
+  GIT_TAG "v1.2.1"
+  OPTIONS
+    "CMAKE_INTERPROCEDURAL_OPTIMIZATION ON"
+    "CMAKE_CXX_STANDARD 17"
+    "MATPLOTPP_BUILD_WITH_PEDANTIC_WARNINGS OFF"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+)
--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -1,5 +1,6 @@
 function(add_header_only_library name header_name)
   add_library(${name} INTERFACE)
+  target_precompile_headers(${name} INTERFACE ${header_name})
   target_compile_definitions(${name} INTERFACE LIBRARY_HEADER_ONLY)
   target_sources(${name}
     INTERFACE
@@ -18,4 +19,20 @@ function(install_libraries)
     LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
   )
   unset(current_subdirectory)
+endfunction()
+
+function(add_boyle_test name source_name)
+  set(test_name ${name}_test)
+  add_executable(${test_name} ${source_name})
+  target_link_libraries(${test_name}
+    PRIVATE
+      cxxopts::cxxopts
+      doctest::doctest
+      Matplot++::matplot
+      ${name}
+  )
+  add_test(
+    NAME ${test_name}
+    COMMAND ${test_name}
+  )
 endfunction()
