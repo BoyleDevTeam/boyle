b2df042a0d44dbba2518fe3d4cec711136eaac03
Houchen Li <houchen_li@hotmail.com>
2024-10-26 20:43:52 +0800

    [Base] refactor project

---
+++ cmake/third_party.cmake
@@ -0,0 +1,119 @@
+include(ExternalProject)
+
+if(CMAKE_C_COMPILER_ID STREQUAL "Clang")
+  find_package(LLVM 19.1.0 REQUIRED CONFIG)
+endif()
+
+find_package(Threads REQUIRED MODULE)
+
+find_package(OpenMP REQUIRED MODULE)
+
+CPMAddPackage(
+  NAME Boost
+  VERSION 1.89.0
+  URL https://github.com/boostorg/boost/releases/download/boost-1.89.0/boost-1.89.0-cmake.7z
+  URL_HASH SHA256=252149f583054515b10688855b24ca1c16072b5118e603b8fe017894d319c262
+  OPTIONS
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+    "CMAKE_UNITY_BUILD OFF"
+    "BUILD_SHARED_LIBS OFF"
+    "BOOST_RUNTIME_LINK static"
+    "BOOST_ENABLE_CMAKE ON"
+    "BOOST_SKIP_INSTALL_RULES OFF"
+    "BOOST_INCLUDE_LIBRARIES headers\\\;serialization"
+)
+
+CPMAddPackage(
+  NAME cxxopts
+  VERSION 3.3.1
+  URL https://github.com/jarro2783/cxxopts/archive/refs/tags/v3.3.1.tar.gz
+  URL_HASH SHA256=3bfc70542c521d4b55a46429d808178916a579b28d048bd8c727ee76c39e2072
+  OPTIONS
+    "CXXOPTS_ENABLE_INSTALL ON"
+)
+
+CPMAddPackage(
+  NAME spdlog
+  VERSION 1.15.3
+  URL https://github.com/gabime/spdlog/archive/refs/tags/v1.15.3.tar.gz
+  URL_HASH SHA256=15a04e69c222eb6c01094b5c7ff8a249b36bb22788d72519646fb85feb267e67
+  OPTIONS
+    "CMAKE_POLICY_VERSION_MINIMUM 3.10"
+    "SPDLOG_BUILD_SHARED OFF"
+    "SPDLOG_INSTALL ON"
+    "SPDLOG_USE_STD_FORMAT ON"
+)
+set_target_properties(spdlog spdlog_header_only
+  PROPERTIES
+    INTERFACE_COMPILE_DEFINITIONS "SPDLOG_ACTIVE_LEVEL=SPDLOG_LEVEL_TRACE"
+)
+
+CPMAddPackage(
+  NAME doctest
+  VERSION 2.4.12
+  URL https://github.com/doctest/doctest/archive/refs/tags/v2.4.12.tar.gz
+  URL_HASH SHA256=73381c7aa4dee704bd935609668cf41880ea7f19fa0504a200e13b74999c2d70
+  PATCHES
+    ${CMAKE_SOURCE_DIR}/cmake/patches/doctest.patch
+  OPTIONS
+    "CMAKE_POLICY_VERSION_MINIMUM 3.10"
+    "CMAKE_POLICY_DEFAULT_CMP0069 NEW"
+    "DOCTEST_WITH_MAIN_IN_STATIC_LIB ON"
+    "DOCTEST_NO_INSTALL ON"
+)
+set_target_properties(doctest
+  PROPERTIES
+    INTERFACE_COMPILE_DEFINITIONS "DOCTEST_CONFIG_SUPER_FAST_ASSERTS"
+)
+
+include(${CMAKE_SOURCE_DIR}/cmake/msft_proxy.cmake)
+
+include(${CMAKE_SOURCE_DIR}/cmake/OpenBLAS.cmake)
+
+include(${CMAKE_SOURCE_DIR}/cmake/pocketfft.cmake)
+
+CPMAddPackage(
+  NAME qdldl
+  VERSION 0.1.9
+  URL https://github.com/osqp/qdldl/archive/refs/tags/v0.1.9.tar.gz
+  URL_HASH SHA256=7d1285b2db15cf2730dc83b3d16ed28412f558591108cca4f28d4438bf72ceed
+  OPTIONS
+    "CMAKE_POLICY_DEFAULT_CMP0069 NEW"
+    "QDLDL_BUILD_STATIC_LIB OFF"
+    "QDLDL_BUILD_SHARED_LIB OFF"
+    "QDLDL_LONG OFF"
+    "QDLDL_FLOAT OFF"
+    "QDLDL_BUILD_DEMO_EXE OFF"
+    "QDLDL_UNITTESTS OFF"
+)
+
+CPMAddPackage(
+  NAME osqp
+  VERSION 1.0.0
+  URL https://github.com/osqp/osqp/releases/download/v1.0.0/osqp-v1.0.0-src.tar.gz
+  URL_HASH SHA256=ec0bb8fd34625d0ea44274ab3e991aa56e3e360ba30935ae62476557b101c646
+  OPTIONS
+    "CMAKE_POLICY_VERSION_MINIMUM 3.10"
+    "CMAKE_COMPILE_WARNING_AS_ERROR OFF"
+    "OSQP_BUILD_SHARED_LIB OFF"
+    "OSQP_USE_LONG OFF"
+    "OSQP_USE_FLOAT OFF"
+    "OSQP_BUILD_DEMO_EXE OFF"
+    "OSQP_ENABLE_PRINTING OFF"
+    "OSQP_ENABLE_PROFILING OFF"
+    "OSQP_ENABLE_INTERRUPT OFF"
+)
+
+CPMAddPackage(
+  NAME Matplot++
+  VERSION 1.2.2
+  URL https://github.com/alandefreitas/matplotplusplus/archive/refs/tags/v1.2.2.tar.gz
+  URL_HASH SHA256=c7434b4fea0d0cc3508fd7104fafbb2fa7c824b1d2ccc51c52eaee26fc55a9a0
+  PATCHES
+    ${CMAKE_SOURCE_DIR}/cmake/patches/matplot++.patch
+  OPTIONS
+    "MATPLOTPP_BUILD_SHARED_LIBS OFF"
+    "MATPLOTPP_BUILD_TESTS OFF"
+    "MATPLOTPP_BUILD_EXAMPLES OFF"
+    "CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS} -Wno-ignored-attributes"
+)
---
+++ src/boyle/math/sparse/lil_matrix.hpp
@@ -0,0 +1,270 @@
+/**
+ * @file lil_matrix.hpp
+ * @author Houchen Li (houchen_li@hotmail.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-10-20
+ *
+ * @copyright Copyright (c) 2023 Boyle Development Team
+ *            All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include <algorithm>
+#include <concepts>
+#include <utility>
+
+#include "boost/serialization/access.hpp"
+
+#include "boyle/math/concepts.hpp"
+#include "boyle/math/sparse/dok_matrix.hpp"
+#include "boyle/math/sparse/dok_vector.hpp"
+
+#ifndef BOYLE_USE_BOOST_UNORDERED
+#include "boost/serialization/unordered_map.hpp"
+#include <unordered_map>
+#else
+#include "boost/unordered/unordered_flat_map.hpp"
+#endif
+
+namespace boyle::math {
+
+template <ScalarArithmetic Scalar = double, std::integral Index = int>
+class [[nodiscard]] LilMatrix final {
+    friend class boost::serialization::access;
+
+  public:
+    using value_type = Scalar;
+    using index_type = Index;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using dictionary_type =
+#ifndef BOYLE_USE_BOOST_UNORDERED
+        std::unordered_map<index_type, std::unordered_map<index_type, value_type>>;
+#else
+        boost::unordered::unordered_flat_map<
+            index_type, boost::unordered::unordered_flat_map<index_type, value_type>>;
+#endif
+
+    LilMatrix() noexcept = default;
+    LilMatrix(const LilMatrix& other) noexcept = default;
+    auto operator=(const LilMatrix& other) noexcept -> LilMatrix& = default;
+    LilMatrix(LilMatrix&& other) noexcept = default;
+    auto operator=(LilMatrix&& other) noexcept -> LilMatrix& = default;
+    ~LilMatrix() noexcept = default;
+
+    [[using gnu: always_inline]]
+    explicit LilMatrix(size_type nrows, size_type ncols) noexcept
+        : m_nrows{nrows}, m_ncols{ncols} {}
+
+    [[using gnu: always_inline]]
+    LilMatrix(const DokMatrix<value_type, index_type>& dok_matrix) noexcept
+        : m_nrows{dok_matrix.nrows()}, m_ncols{dok_matrix.ncols()}, m_nnzs{dok_matrix.nnzs()} {
+        for (const auto& [index_pair, value] : dok_matrix.dictionary()) {
+            m_row_dictionaries[index_pair.row].emplace(index_pair.col, value);
+        }
+    }
+
+    [[using gnu: ]] operator DokMatrix<value_type, index_type>() const noexcept {
+        DokMatrix<value_type, index_type> dok_matrix{m_nrows, m_ncols};
+        dok_matrix.reserve(m_nnzs);
+        for (const auto& [row, row_dictionary] : m_row_dictionaries) {
+            for (const auto& [col, value] : row_dictionary) {
+                dok_matrix.updateCoeff(row, col, value);
+            }
+        }
+        return dok_matrix;
+    }
+
+    [[using gnu: pure, always_inline, leaf]] [[nodiscard]]
+    auto nrows() const noexcept -> size_type {
+        return m_nrows;
+    }
+
+    [[using gnu: pure, always_inline, leaf]] [[nodiscard]]
+    auto ncols() const noexcept -> size_type {
+        return m_ncols;
+    }
+
+    [[using gnu: pure, always_inline, leaf]] [[nodiscard]]
+    auto nnzs() const noexcept -> size_type {
+        return m_nnzs;
+    }
+
+    [[using gnu: ]]
+    auto resize(size_type nrows, size_type ncols) noexcept -> void {
+#ifndef BOYLE_USE_BOOST_UNORDERED
+        using std::erase_if;
+#else
+        using boost::unordered::erase_if;
+#endif
+        erase_if(
+            m_row_dictionaries,
+            [nrows](const auto& row_dictionary) constexpr noexcept -> bool {
+                return row_dictionary.first >= static_cast<index_type>(nrows);
+            }
+        );
+        for (auto& [row, row_dictionary] : m_row_dictionaries) {
+            erase_if(row_dictionary, [ncols](const auto& item) constexpr noexcept -> bool {
+                return item.first >= static_cast<index_type>(ncols);
+            });
+        }
+        erase_if(m_row_dictionaries, [](const auto& row_dictionary) constexpr noexcept -> bool {
+            return row_dictionary.second.empty();
+        });
+        m_nnzs = 0;
+        for (const auto& [row, row_dictionary] : m_row_dictionaries) {
+            m_nnzs += row_dictionary.size();
+        }
+        m_nrows = nrows;
+        m_ncols = ncols;
+        return;
+    }
+
+    [[using gnu: always_inline]]
+    auto reserve(size_type capacity) noexcept -> void {
+        m_row_dictionaries.reserve(capacity);
+        return;
+    }
+
+    [[using gnu: always_inline]]
+    auto clear() noexcept -> void {
+        m_nnzs = 0;
+        m_row_dictionaries.clear();
+        return;
+    }
+
+    [[using gnu: always_inline]]
+    auto compress() noexcept -> void {
+        return;
+    }
+
+    [[using gnu: pure, flatten, leaf, hot]]
+    auto coeff(size_type row, size_type col) const noexcept -> value_type {
+        if (row >= m_nrows || col >= m_ncols) [[unlikely]] {
+            return static_cast<value_type>(0.0);
+        }
+        if (const auto search_row = m_row_dictionaries.find(static_cast<index_type>(row));
+            search_row != m_row_dictionaries.cend()) {
+            if (const auto search_col = search_row->second.find(static_cast<index_type>(col));
+                search_col != search_row->second.cend()) {
+                return search_col->second;
+            }
+        }
+        return static_cast<value_type>(0.0);
+    }
+
+    [[using gnu: flatten, leaf, hot]]
+    auto updateCoeff(size_type row, size_type col, const_reference value) noexcept -> void {
+        if (row >= m_nrows || col >= m_ncols) [[unlikely]] {
+            return;
+        }
+        if (auto search_row = m_row_dictionaries.find(static_cast<index_type>(row));
+            search_row != m_row_dictionaries.end()) {
+            if (auto search_col = search_row->second.find(static_cast<index_type>(col));
+                search_col != search_row->second.end()) {
+                search_col->second = value;
+            } else {
+                m_nnzs += 1;
+                search_row->second.emplace(static_cast<index_type>(col), value);
+            }
+        } else {
+            if (value != value_type(0.0)) {
+                m_nnzs += 1;
+                m_row_dictionaries.emplace(
+                    static_cast<index_type>(row),
+                    decltype(search_row->second){{static_cast<index_type>(col), value}}
+                );
+            }
+        }
+        return;
+    }
+
+    [[using gnu: flatten, leaf, hot]]
+    auto eraseCoeff(size_type row, size_type col) noexcept -> void {
+        if (row >= m_nrows || col >= m_ncols) [[unlikely]] {
+            return;
+        }
+        if (auto search_row = m_row_dictionaries.find(static_cast<index_type>(row));
+            search_row != m_row_dictionaries.end()) {
+            if (auto search_col = search_row->second.find(static_cast<index_type>(col));
+                search_col != search_row->second.end()) {
+                m_nnzs -= 1;
+                if (search_row->second.size() != 1) {
+                    search_row->second.erase(search_col);
+                } else {
+                    m_row_dictionaries.erase(search_row);
+                }
+            }
+        }
+        return;
+    }
+
+    [[using gnu: pure, always_inline, hot]] [[nodiscard]]
+    auto operator[](size_type row, size_type col) const noexcept -> value_type {
+        return coeff(row, col);
+    }
+
+    [[using gnu: flatten, leaf, hot]]
+    auto updateRow(size_type row, const DokVector<value_type, index_type>& row_vector) noexcept
+        -> void {
+#ifndef BOYLE_USE_BOOST_UNORDERED
+        using std::erase_if;
+#else
+        using boost::unordered::erase_if;
+#endif
+        if (row >= m_nrows) [[unlikely]] {
+            return;
+        }
+        if (auto search = m_row_dictionaries.find(static_cast<index_type>(row));
+            search != m_row_dictionaries.end()) {
+            m_nnzs -= search->second.size();
+            m_row_dictionaries.erase(search);
+        }
+        auto row_dictionary{row_vector.dictionary()};
+        erase_if(row_dictionary, [this](const auto& item) constexpr noexcept -> bool {
+            return item.first >= static_cast<index_type>(m_ncols);
+        });
+        m_nnzs += row_dictionary.size();
+        m_row_dictionaries.emplace(static_cast<index_type>(row), std::move(row_dictionary));
+        return;
+    }
+
+    [[using gnu: flatten, leaf, hot]]
+    auto eraseRow(size_type row) noexcept -> void {
+        if (row >= m_nrows) [[unlikely]] {
+            return;
+        }
+        if (auto search = m_row_dictionaries.find(static_cast<index_type>(row));
+            search != m_row_dictionaries.end()) {
+            m_nnzs -= search->second.size();
+            m_row_dictionaries.erase(search);
+        }
+    }
+
+    [[using gnu: always_inline, leaf]]
+    auto row_dictionaries() const noexcept -> const dictionary_type& {
+        return m_row_dictionaries;
+    }
+
+  private:
+    [[using gnu: always_inline]]
+    auto serialize(auto& archive, [[maybe_unused]] const unsigned int version) noexcept -> void {
+        archive & m_nrows;
+        archive & m_ncols;
+        archive & m_nnzs;
+        archive & m_row_dictionaries;
+        return;
+    }
+
+    size_type m_nrows{0};
+    size_type m_ncols{0};
+    size_type m_nnzs{0};
+    dictionary_type m_row_dictionaries{};
+};
+
+} // namespace boyle::math
